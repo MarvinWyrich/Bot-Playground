25b75db4fc81374f5c697dfaa8636af3185c9549
Marvin Wyrich <mail@marvin-wyrich.de>
2019-04-26 13:01:16 +0200

    Add Caller.java to test unused method parameter with multiple files

---
+++ src/main/java/smells/unusedmethodparameter/Caller.java
@@ -0,0 +1,12 @@
+package smells.unusedmethodparameter;
+
+/**
+ * Unused method parameters confuse developers and should therefore be removed.
+ */
+public class Caller {
+
+	protected int callSth() {
+		return new UnusedMethodParameter().calculateSomething(1, 2, 3);
+	}
+
+}
